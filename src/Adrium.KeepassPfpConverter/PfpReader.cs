5541d5b3edef574583f3a2702fa93b3f90920aa8
Adrian <adrian.dev@kousz.ch>
2019-02-25 12:45:26 +0100

    Add PfpReader and improve command handline in Program

---
+++ src/Adrium.KeepassPfpConverter/PfpReader.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Adrium.KeepassPfpConverter
+{
+	public class PfpReader
+	{
+		private readonly string masterPassword;
+
+		public PfpReader(string masterPassword)
+		{
+			this.masterPassword = masterPassword;
+		}
+
+		public IList<EntryObject> GetEntries(string backupJson)
+		{
+			var backup = JsonConvert.DeserializeObject<BackupObject>(backupJson);
+
+			var crypto = new Crypto(masterPassword, backup.data["salt"]);
+			var password = new Password(crypto);
+
+			var result = new List<EntryObject>();
+
+			foreach (var item in backup.data) {
+				if (!item.Key.StartsWith("site:"))
+					continue;
+
+				var json = crypto.Decrypt(item.Value);
+				var entry = JsonConvert.DeserializeObject<EntryObject>(json);
+
+				if (entry.type != null)
+					entry.password = password.GetPassword(entry);
+
+				result.Add(entry);
+			}
+
+			return result;
+		}
+	}
+}
--- src/Adrium.KeepassPfpConverter/Program.cs
+++ src/Adrium.KeepassPfpConverter/Program.cs
@@ -9,62 +9,89 @@ namespace Adrium.KeepassPfpConverter
 	{
 		public static void Main(string[] args)
 		{
-			if (args.Length < 3) {
-				PrintHelp();
+			var commands = new List<Command>();
+			commands.Add(new Command { Name = "decrypt", Args = 3, Usage = "<file> <masterPassword> <output>", Cmd = DecryptCommand });
+
+			var cmdname = args.Length < 1 ? "" : args[0];
+			var command = commands.Find(x => x.Name.Equals(cmdname));
+
+			if (command == default(Command)) {
+				PrintHelp(commands);
 				return;
 			}
 
 			var cmdargs = new string[args.Length - 1];
 			Array.Copy(args, 1, cmdargs, 0, cmdargs.Length);
 
-			try {
-				DecryptCommand(cmdargs);
-			} catch (Org.BouncyCastle.Crypto.InvalidCipherTextException e) {
-				LogError("Decryption not successful. Check master password.\nException: " + e.Message);
+			if (cmdargs.Length < command.Args) {
+				PrintHelp(command);
+				return;
 			}
+
+			command.Cmd(cmdargs);
 		}
 
 		private static void DecryptCommand(string[] args)
 		{
+			string error = null;
+			Exception e = null;
+
+			try {
+				DecryptCommandImpl(args);
+			} catch (Org.BouncyCastle.Crypto.InvalidCipherTextException ex) {
+				e = ex;
+				error = "Decryption not successful. Check master password.";
+			}
+
+			if (e != null) {
+				Console.WriteLine("ERROR: {0}", error);
+				Console.WriteLine("Exception: {0}", e.Message);
+			}
+		}
+
+		private static void DecryptCommandImpl(string[] args)
+		{
 			using (var input = new StreamReader(args[0]))
 			using (var output = new StreamWriter(args[2]))
 			{
+				var masterPassword = args[1];
 				var str = input.ReadToEnd();
-				var backup = JsonConvert.DeserializeObject<BackupObject>(str);
 
-				var crypto = new Crypto(args[1], backup.data["salt"]);
-				var password = new Password(crypto);
-
-				var decrypted = new Dictionary<string, EntryObject>();
-
-				foreach (var item in backup.data) {
-					if (!item.Key.StartsWith("site:"))
-						continue;
-
-					var json = crypto.Decrypt(item.Value);
-					var entry = JsonConvert.DeserializeObject<EntryObject>(json);
-
-					entry.password = password.GetPassword(entry);
-
-					decrypted.Add(item.Key, entry);
-				}
-
-				var outputjson = JsonConvert.SerializeObject(decrypted, Formatting.Indented);
+				var pfpreader = new PfpReader(masterPassword);
+				var entries = pfpreader.GetEntries(str);
+				var outputjson = JsonConvert.SerializeObject(entries, Formatting.Indented);
 				output.WriteLine(outputjson);
 			}
 		}
 
-		private static void PrintHelp()
+		private static void PrintHelp(IList<Command> commands)
 		{
-			var path = System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Assembly.Location;
-			var exe = System.IO.Path.GetFileName(path);
-
-			Console.WriteLine("{0} decrypt <file> <masterPassword> <output>", exe);
+			var exe = GetExe();
+			Console.WriteLine("USAGE:");
+			foreach (var command in commands)
+				Console.WriteLine("{0} {1} {2}", exe, command.Name, command.Usage);
 		}
 
-		private static void LogError(string msg)
+		private static void PrintHelp(Command command)
 		{
-			Console.WriteLine("\u001b[31m" + msg + "\u001b[0m");
+			var exe = GetExe();
+			Console.WriteLine("USAGE:");
+			Console.WriteLine("{0} {1} {2}", exe, command.Name, command.Usage);
+		}
+
+		private static string GetExe()
+		{
+			var path = System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Assembly.Location;
+			var result = System.IO.Path.GetFileName(path);
+			return result;
+		}
+
+		private class Command
+		{
+			public string Name;
+			public int Args;
+			public Action<string[]> Cmd;
+			public string Usage;
 		}
 	}
 }
