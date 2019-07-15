cff41c0350e0f983e14fc937789c59e36ec501ed
Adrian <adrian.dev@kousz.ch>
2019-07-15 22:46:55 +0200

    Update libraries

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyTitle ("KeepassPfpConverter")]
 [assembly: AssemblyDescription ("Import and export backup files of Pain-free Passwords")]
 [assembly: AssemblyProduct("KeePass Plugin")]
-[assembly: AssemblyVersion ("1.3")]
+[assembly: AssemblyVersion ("1.4")]
