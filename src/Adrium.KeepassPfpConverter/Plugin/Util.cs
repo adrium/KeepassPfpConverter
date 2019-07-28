3c14fb6032695d944ffe13ed9153a3c0813f369a
Adrian <adrian.dev@kousz.ch>
2019-07-28 11:54:28 +0200

    Simplify Plugin Util

--- src/Adrium.KeepassPfpConverter/Plugin/Util.cs
+++ src/Adrium.KeepassPfpConverter/Plugin/Util.cs
@@ -1,7 +1,5 @@
 ﻿using Adrium.KeepassPfpConverter.Objects;
 using KeePassLib;
-using KeePassLib.Security;
-using KeePassLib.Utility;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
@@ -15,45 +13,39 @@ namespace Adrium.KeepassPfpConverter.Plugin
 		private const string EmptyPassword = "X";
 		private const string RevisionKey = "Revision";
 
-		private delegate string StringGetter(string key);
-		private delegate void StringSetter(string key, string value);
-
 		public static PwEntry GetKeepassEntry(Crypto crypto, PassEntry entry, MemoryProtectionConfig protect)
 		{
 			var result = new PwEntry(true, true);
-			var strings = result.Strings;
-
-			StringSetter setter = (key, value) =>
-				strings.Set(key, new ProtectedString(protect.GetProtection(key), value));
+			var resultidx = new PwEntryIndexer(result, protect);
 
 			var pw = Password.GetPassword(crypto, entry);
+
 			if (!pw.Equals(EmptyPassword))
-				setter(PwDefs.PasswordField, pw);
+				resultidx[PwDefs.PasswordField] = pw;
 
 			if (!entry.name.Equals(EmptyUsername))
-				setter(PwDefs.UserNameField, entry.name);
+				resultidx[PwDefs.UserNameField] = entry.name;
 
 			if (entry.site.Equals(EmptyUrl)) {
-				setter(PwDefs.TitleField, entry.name);
+				resultidx[PwDefs.TitleField] = entry.name;
 			} else {
-				setter(PwDefs.TitleField, entry.site);
-				setter(PwDefs.UrlField, entry.site);
+				resultidx[PwDefs.TitleField] = entry.site;
+				resultidx[PwDefs.UrlField] = entry.site;
 			}
 
 			var fields = new Dictionary<string, string>();
 			var notes = ParseNotes(entry.notes ?? "", fields);
 
-			if (!string.IsNullOrEmpty(entry.revision))
+			if (!entry.revision.Equals(""))
 				notes = $"{RevisionKey}: {entry.revision}\n{notes}";
 
-			notes = notes.Trim();
-			notes = StrUtil.NormalizeNewLines(notes, true);
+			notes = notes.Replace("\r\n", "\n").Replace("\r", "\n").Replace("\n", "\r\n").Trim();
 
 			if (!notes.Equals(""))
-				setter(PwDefs.NotesField, notes);
+				resultidx[PwDefs.NotesField] = notes;
 
 			foreach (var field in fields)
-				setter(field.Key, field.Value);
+				resultidx[field.Key] = field.Value;
 
 			return result;
 		}
@@ -61,36 +53,27 @@ namespace Adrium.KeepassPfpConverter.Plugin
 		public static PassEntry GetPfpEntry(Crypto crypto, PwEntry pwEntry)
 		{
 			var result = new StoredEntry();
-			var fields = new Dictionary<string, string>();
-			var value = "";
+			var fields = new SortedDictionary<string, string> {
+				{ PwDefs.UserNameField, EmptyUsername },
+				{ PwDefs.PasswordField, EmptyPassword },
+				{ PwDefs.UrlField, EmptyUrl },
+				{ PwDefs.NotesField, "" },
+				{ RevisionKey, "" }
+			};
 
-			foreach (var field in pwEntry.Strings) {
-				var key = field.Key;
-				value = field.Value.ReadString();
+			foreach (var field in pwEntry.Strings)
+				if (!field.Value.ReadString().Equals(""))
+					fields[field.Key] = field.Value.ReadString();
 
-				if (key.Equals(PwDefs.UrlField))
-					value = GetSitePart(value);
-
-				if (key.Equals(PwDefs.NotesField))
-					value = ParseNotes(value, fields);
-
-				if (value.Equals(""))
-					continue;
-
-				fields.Add(key, value);
-			}
-
-			StringGetter getter = key =>
-				fields.ContainsKey(key) ? fields[key] : null;
-
-			var notes = "";
-			var textnotes = getter(PwDefs.NotesField) ?? "";
+			result.notes = "%fields%";
+			result.notes += ParseNotes(fields[PwDefs.NotesField], fields)
+				.Replace("\r\n", "\n").Replace("\r", "\n");
 
 			result.type = "stored";
-			result.name = getter(PwDefs.UserNameField) ?? EmptyUsername;
-			result.password = getter(PwDefs.PasswordField) ?? EmptyPassword;
-			result.site = getter(PwDefs.UrlField) ?? EmptyUrl;
-			result.revision = getter(RevisionKey) ?? "";
+			result.site = fields[PwDefs.UrlField];
+			result.name = fields[PwDefs.UserNameField];
+			result.password = fields[PwDefs.PasswordField];
+			result.revision = fields[RevisionKey];
 
 			fields.Remove(PwDefs.TitleField);
 			fields.Remove(PwDefs.UserNameField);
@@ -100,18 +83,15 @@ namespace Adrium.KeepassPfpConverter.Plugin
 			fields.Remove(RevisionKey);
 
 			foreach (var field in fields) {
-				value = field.Value;
-				value = StrUtil.NormalizeNewLines(value, false);
-				value = value.Replace('\n', ' ');
-				notes += $"{field.Key}: {value}\n";
+				var value = field.Value.Replace("\r\n", " ").Replace("\n", " ");
+				result.notes = result.notes.Replace("%fields%", $"{field.Key}: {value}\n%fields%");
 			}
 
-			notes += textnotes;
-			notes = notes.Trim();
-			notes = StrUtil.NormalizeNewLines(notes, false);
+			result.site = GetSitePart(result.site);
+			result.notes = result.notes.Replace("%fields%", "").Trim();
 
-			if (!notes.Equals(""))
-				result.notes = notes;
+			if (result.notes.Equals(""))
+				result.notes = null;
 
 			return result;
 		}
@@ -127,11 +107,10 @@ namespace Adrium.KeepassPfpConverter.Plugin
 			while ((line = reader.ReadLine()) != null) {
 				if (parsing) {
 					var match = matcher.Match(line);
-					if (match.Success) {
-						dict.Add(match.Groups[1].Value, match.Groups[2].Value);
-					} else {
+					if (match.Success)
+						dict[match.Groups[1].Value] = match.Groups[2].Value;
+					else
 						parsing = false;
-					}
 				}
 
 				if (!parsing)
