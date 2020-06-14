8703bd60d7759a67285582df59a631adf696e537
Adrian <adrian.dev@kousz.ch>
2020-06-14 19:00:12 +0200

    Set version 1.6

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyTitle ("PfpConverter")]
 [assembly: AssemblyDescription ("Import and export backup files of Pain-free Passwords")]
 [assembly: AssemblyProduct("KeePass Plugin")]
-[assembly: AssemblyVersion ("1.5")]
+[assembly: AssemblyVersion ("1.6")]
